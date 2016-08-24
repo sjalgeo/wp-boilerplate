9296353d9da56a71cfbd8ef231cb15cc9d7bd1cf
Stephen Algeo <stephen.algeo@gmail.com>
2016-08-24 12:12:43 +0100

    Add Theme Name Variable to Build

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,30 @@
-var gulp          = require('gulp');
-var autoprefixer  = require('gulp-autoprefixer');
-var sass          = require('gulp-sass');
-
-gulp.task('default', function(){
-  // default task
-  console.log('default gulp task')
-});
+var gulp          	= require('gulp');
+var replace 		= require('gulp-replace');
+var autoprefixer  	= require('gulp-autoprefixer');
+var sass          	= require('gulp-sass');
 
 var sassFiles = './sass/**/*.sass';
 
+var themeNamePlaceholder = '{{{THEME_NAME}}}';
+var currentThemeName = 'UPDATE THEME NAME';
+
+
 // Let's get sassy; prefix and minify our stylesheet
 gulp.task('sass', function(){
-  gulp.src(sassFiles)
-    .pipe(sass.sync().on('error', sass.logError))
-    .pipe(autoprefixer('last 2 versions'))
-    .pipe(gulp.dest('./'));
+	gulp.src(sassFiles)
+	  .pipe(sass.sync().on('error', sass.logError))
+	  .pipe(autoprefixer('last 2 versions'))
+	  .pipe(replace(themeNamePlaceholder, currentThemeName))
+	  .pipe(gulp.dest('./'));
 });
 
 // Watch
 gulp.task('sass:watch', function(){
-  gulp.watch(sassFiles, ['sass'])
+	gulp.watch(sassFiles, ['sass'])
 });
 
 // Just run `$ gulp` in the terminal to compile everything and watch sass
-gulp.task('default', ['sass', 'sass:watch']);
+gulp.task('default', [
+	// 'sass:watch',
+	'sass'
+]);
