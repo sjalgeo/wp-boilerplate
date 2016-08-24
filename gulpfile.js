88f73f14745eef1e768dbd279b2385a015253767
Stephen Algeo <stephen.algeo@gmail.com>
2016-08-24 13:41:37 +0100

    Add Config Task to Gulp Build

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,13 @@ var gulp          	= require('gulp');
 var replace 		= require('gulp-replace');
 var autoprefixer  	= require('gulp-autoprefixer');
 var sass          	= require('gulp-sass');
+var fs				= require('fs');
 
 var sassFiles = './sass/**/*.sass';
 
 var themeNamePlaceholder = '{{{THEME_NAME}}}';
-var currentThemeName = 'UPDATE THEME NAME';
+var currentThemeName = 'UPDATED THEME NAME';
+var currentVersion = '0.1.0';
 
 
 // Let's get sassy; prefix and minify our stylesheet
@@ -23,8 +25,26 @@ gulp.task('sass:watch', function(){
 	gulp.watch(sassFiles, ['sass'])
 });
 
+// Config File
+gulp.task('config', function() {
+	var directory = 'config';
+	var filename = 'version.php';
+	var filepath = directory + '/' + filename;
+
+	try {
+		fs.mkdir(directory, null, function(){});
+	} catch (e) {
+		if (e.code !== 'EEXIST') throw e;
+	}
+
+	fs.open(filepath, 'w');
+	var phpConstants = "define";
+	fs.writeFile(filepath, currentVersion);
+});
+
 // Just run `$ gulp` in the terminal to compile everything and watch sass
 gulp.task('default', [
 	// 'sass:watch',
+	'config',
 	'sass'
 ]);
